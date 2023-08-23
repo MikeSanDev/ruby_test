1afe9772b3cb1db023b888f3340f97c0004a57d9
MikeSanDev <mjs070594@gmail.com>
2023-08-22 21:19:46 -0500

    first commit

---
+++ howdy.rb
@@ -0,0 +1,8 @@
+my_string = "Hello World!"
+pp my_string
+new_num = 1.ordinalize
+pp new_num
+
+require './goodbye.rb'
+# can use active support methods like integer method - ordinalize or string method singularize
+require 'active_support/all'
