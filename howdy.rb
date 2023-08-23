fff896080a633f8be5e19e9a95f8b5df4e498f58
MikeSanDev <mjs070594@gmail.com>
2023-08-22 21:21:34 -0500

    new

--- howdy.rb
+++ howdy.rb
@@ -1,7 +1,5 @@
 my_string = "Hello World!"
-pp my_string
-new_num = 1.ordinalize
-pp new_num
+pp my_string  
 
 require './goodbye.rb'
 # can use active support methods like integer method - ordinalize or string method singularize
